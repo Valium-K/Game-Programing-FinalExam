dba21f14072bf1cd8a48bfd1159d32c5a499cde3
Valium-K <lschan96@gmail.com>
2019-11-28 20:00:54 +0900

    필수기능 추가 및 정리

---
+++ _Project/README.md
@@ -0,0 +1,11 @@
+﻿# __유니티 프로젝트 실행법__
+> Project Files.zip을 압축해제 후<br/>
+> Resource 폴더 -> 최신 날짜 -> Assets.zip 압축해제 후<br/>
+> 압축해제된 Assets 폴더를 압축해제된 Project Files폴더에 넣는다. (Assets 이름 유지!)<br/>
+> 이후 Project Files/Assets/Scenes/씬이름.unity 실행
+
+### __기능 추가 후 푸시 규칙__
+> 해당 날짜 폴더안에 수정된 Assets폴더를 압축해 올린다.<br/>
+> 해당 날짜 폴더안에 README.md를 만든 후 기능 추가, 수정 등을 적는다.
+
+-----------------
---
+++ _Project/Resource/11.24/Assets/Scripts/backup - without Comment/EagleMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleMovement : MonoBehaviour
+{
+    public float moveTime = 10.5f;
+    private float tempTime = 0f;
+    private bool switchDir = false;
+
+    private void Start() {
+        tempTime = moveTime;
+    }
+    void FixedUpdate()
+    {
+        if(tempTime <= 0f)
+        {
+            tempTime = moveTime;
+            switchDir = !switchDir;
+        }
+        else
+        {
+            tempTime -= Time.deltaTime;
+        }
+
+        if(switchDir)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.1f);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.1f);
+        }
+    }
+}
